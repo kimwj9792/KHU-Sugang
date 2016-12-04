1a2dfcef05259aff90c2432a87320a88ff727daf
kimwj9792 <kuj0902@gmail.com>
2016-12-04 18:34:09 +0900

    Database Config

---
+++ app/config.php
@@ -0,0 +1,13 @@
+<?php
+set_time_limit(0);
+error_reporting(0);
+date_default_timezone_set('Asia/Seoul');
+
+$config['db_address'] = '';
+$config['db_user'] = '';
+$config['db_password'] = '';
+$config['db_name'] = '';
+$config['db_port'] = '3306';
+$config['db_prefix'] = '';
+
+$now = date('Y-m-d H:i:s');
